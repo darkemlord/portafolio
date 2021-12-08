428a045ee882a251a56160ccb4f86536f9432b90
emanuel caro <darkemlord@gmail.com>
2021-12-09 00:54:49 +0900

    a lot of changes in the navbar css

--- source/assets/javascripts/canvas.js
+++ source/assets/javascripts/canvas.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
+// export the complete canvas file to the index.js to display all
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer({
