42842ef16104d06572f75cbeef809abe3d325c0a
emanuel caro <darkemlord@gmail.com>
2021-12-09 18:12:47 +0900

    messages updated

--- source/assets/javascripts/typed.js
+++ source/assets/javascripts/typed.js
@@ -1,6 +1,6 @@
 import Typed from 'typed.js';
 
 const typed = new Typed('#typed-text', {
-  strings: ['Passionate student, enjoy designing and developing user interfaces!', 'I\'m ready for the next challenge!'],
+  strings: ['Passionate student, enjoy designing and developing user interfaces!', 'I\'m ready for the next challenge! So please let me know what\'s your story!'],
   typeSpeed: 80
 });
