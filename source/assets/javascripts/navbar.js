e643c744dd3e9d250b7c1f27901b43a3c42f9b51
emanuel caro <darkemlord@gmail.com>
2021-12-09 18:02:15 +0900

    typedjs working

--- source/assets/javascripts/navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector('.emanuel-navbar');
-const logo = document.querySelector('.logo');
-
-const changeNavbarStyle = () => {
-  logo.style.background = 'red';
-};
-
-navbar.addEventListener('onmouseover', changeNavbarStyle);
---
+++ source/assets/javascripts/typed.js
@@ -0,0 +1,6 @@
+import Typed from 'typed.js';
+
+const typed = new Typed('#typed-text', {
+  strings: ['Passionate student, enjoy designing and developing user interfaces!', 'I\'m ready for the next challenge!'],
+  typeSpeed: 80
+});
