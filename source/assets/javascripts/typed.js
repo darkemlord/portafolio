ee533fac594fe4e7c47478e8889b09248179767b
emanuel caro <darkemlord@gmail.com>
2021-12-09 18:24:18 +0900

    shadow text added on the paragraph

--- source/assets/javascripts/typed.js
+++ source/assets/javascripts/typed.js
@@ -1,5 +1,6 @@
 import Typed from 'typed.js';
 
+// eslint-disable-next-line no-unused-vars
 const typed = new Typed('#typed-text', {
   strings: ['Passionate student, enjoy designing and developing user interfaces!', 'I\'m ready for the next challenge! So please let me know what\'s your story!'],
   typeSpeed: 80
