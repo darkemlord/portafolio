2533ea51a811a04368a0e502ef89ad0baed5baaa
emanuel caro <darkemlord@gmail.com>
2021-12-13 10:59:48 +0900

    new updates in the footer and banner text

--- source/assets/javascripts/typed.js
+++ source/assets/javascripts/typed.js
@@ -2,6 +2,6 @@ import Typed from 'typed.js';
 
 // eslint-disable-next-line no-unused-vars
 const typed = new Typed('#typed-text', {
-  strings: ['Passionate student, enjoy designing and developing user interfaces!', 'I\'m ready for the next challenge! So please let me know what\'s your story!'],
+  strings: ['I am a passionate student who enjoys creating web applications that offer an excellent user experience.', 'I\'m ready for the next challenge! So please let me know what your story is!'],
   typeSpeed: 80
 });
